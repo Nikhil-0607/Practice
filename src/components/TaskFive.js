b2fd384373ceb2d4b5bb36547013f601cd6e6ba8
Nikhil Sharma <nikhilshrma0607@gmail.com>
2024-11-24 09:48:20 +0530

    Form Data by UseEffect on cards

--- src/components/TaskFive.js
+++ src/components/TaskFive.js
@@ -1,43 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Space } from "antd";
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const TaskFive = () => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(()=>{
+    axios.get("http://localhost:8090/api/customers")
+    .then((response)=>{
+      // response.json();
+      console.log(response);
+      setData(response.data);
+    })
+    .catch((error)=>console.error("error fetching data",error));
+  },[]);
   
-    const data = [
-      {
-          "email": "nikhilshrma0607@gmail.com",
-          "name": "NIKHIL SHARMA",
-          "mobile": "08538980844",
-          "gender": "male",
-          "state": "jharkhand",
-          "aadhar": "55222445",
-          "createdAt": "2024-11-24T03:32:16.374Z",
-          "updatedAt": "2024-11-24T03:32:16.374Z",
-          "id": "67429e40b991e2415907f16a"
-      },
-      {
-          "email": "saurabhdhauria@gmail.com",
-          "name": "SARAV DHAURIA",
-          "mobile": "8538980844",
-          "gender": "male",
-          "state": "jharkhand",
-          "aadhar": "55222445",
-          "createdAt": "2024-11-24T03:41:03.443Z",
-          "updatedAt": "2024-11-24T03:41:03.443Z",
-          "id": "6742a04fb991e2415907f16e"
-      },
-      {
-          "email": "saurabhdhauria@gmail.com",
-          "name": "HANS MAHATO",
-          "mobile": "8538980844",
-          "gender": "male",
-          "state": "jharkhand",
-          "aadhar": "55222445",
-          "createdAt": "2024-11-24T03:41:29.505Z",
-          "updatedAt": "2024-11-24T03:41:29.505Z",
-          "id": "6742a069b991e2415907f170"
-      }
-  ];
+  //   const data = [
+  //     {
+  //         "email": "nikhilshrma0607@gmail.com",
+  //         "name": "NIKHIL SHARMA",
+  //         "mobile": "08538980844",
+  //         "gender": "male",
+  //         "state": "jharkhand",
+  //         "aadhar": "55222445",
+  //         "createdAt": "2024-11-24T03:32:16.374Z",
+  //         "updatedAt": "2024-11-24T03:32:16.374Z",
+  //         "id": "67429e40b991e2415907f16a"
+  //     },
+  //     {
+  //         "email": "saurabhdhauria@gmail.com",
+  //         "name": "SARAV DHAURIA",
+  //         "mobile": "8538980844",
+  //         "gender": "male",
+  //         "state": "jharkhand",
+  //         "aadhar": "55222445",
+  //         "createdAt": "2024-11-24T03:41:03.443Z",
+  //         "updatedAt": "2024-11-24T03:41:03.443Z",
+  //         "id": "6742a04fb991e2415907f16e"
+  //     },
+  //     {
+  //         "email": "saurabhdhauria@gmail.com",
+  //         "name": "HANS MAHATO",
+  //         "mobile": "8538980844",
+  //         "gender": "male",
+  //         "state": "jharkhand",
+  //         "aadhar": "55222445",
+  //         "createdAt": "2024-11-24T03:41:29.505Z",
+  //         "updatedAt": "2024-11-24T03:41:29.505Z",
+  //         "id": "6742a069b991e2415907f170"
+  //     }
+  // ];
   return (
     <div>
       <Space direction="horizontal" size={20}>
